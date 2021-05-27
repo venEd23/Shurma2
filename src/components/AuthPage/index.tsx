d27435fdd43c0540fdef3519675b06907be34777
venEd <synthol94@yahoo.com>
2021-05-27 14:04:26 +0300

    initial commit

--- src/components/AuthPage/index.tsx
+++ src/components/AuthPage/index.tsx
@@ -1,14 +1,12 @@
 import React, { FC } from "react";
 import style from "./AuthPage.module.scss";
 
-const AuthPage: FC = () => {
-    return (
-        <div className={style.auth_page_wrapper}>
-            <Header />
-            <div>Auth Page</div>
-            <Footer />
-        </div>
-    );
-};
+const AuthPage: FC = () => (
+  <div className={style.auth_page_wrapper}>
+    <Header />
+    <div>Auth Page</div>
+    <Footer />
+  </div>
+);
 
 export default AuthPage;
